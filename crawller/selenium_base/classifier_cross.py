ce4a52e09a9c6a522f3fd6412bb5d6c1522fd6e9
Sanqiang <zhaosanqiang916@gmail.com>
2017-02-02 18:28:03 -0500

    add pickle

--- crawller/selenium_base/classifier_cross.py
+++ crawller/selenium_base/classifier_cross.py
@@ -8,6 +8,7 @@ from sklearn import linear_model
 from joblib import Parallel, delayed
 import multiprocessing
 import pickle
+import os
 
 num_cpus = multiprocessing.cpu_count()
 
@@ -18,46 +19,61 @@ data_text_x = {}
 data_stat_x = {}
 data_y = {}
 
-for category in obj:
-    if category not in data_text_x:
-        print(category)
-        data_text_x[category] = []
-        data_stat_x[category] = []
-        data_y[category] = []
+if os.path.exists("data_text_x"):
+    f_data_text_x = open("data_text_x", "rb")
+    f_data_stat_x = open("data_stat_x", "rb")
+    f_data_y = open("f_data_y", "rb")
+    data_text_x = pickle.load(f_data_text_x)
+    data_stat_x = pickle.load(f_data_stat_x)
+    data_y = pickle.load(f_data_y)
+else:
+    for category in obj:
+        if category not in data_text_x:
+            if len(data_text_x) == 2:
+                break
+            print(category)
+            data_text_x[category] = []
+            data_stat_x[category] = []
+            data_y[category] = []
 
-    data_entries = obj[category]
-    for data_entry in data_entries:
-        for label in data_entry:
-            text = data_entry[label]
+        data_entries = obj[category]
+        for data_entry in data_entries:
+            for label in data_entry:
+                text = data_entry[label]
 
-            data_text_x[category].append(text)
-            data_y[category].append(int(label))
+                data_text_x[category].append(text)
+                data_y[category].append(int(label))
 
-            sent_avg_len = 0
-            sent_cnt = 1
-            word_avg_len = 0
-            word_cnt = 1
-            sents = sent_tokenize(text)
-            for sent in sents:
-                sent_avg_len += len(word_tokenize(sent))
-                sent_cnt += 1
-                for word in word_tokenize(sent):
-                    word_avg_len += len(word)
-                    word_cnt += 1
-            sent_avg_len /= sent_cnt
-            word_avg_len /= word_cnt
+                sent_avg_len = 0
+                sent_cnt = 1
+                word_avg_len = 0
+                word_cnt = 1
+                sents = sent_tokenize(text)
+                for sent in sents:
+                    sent_avg_len += len(word_tokenize(sent))
+                    sent_cnt += 1
+                    for word in word_tokenize(sent):
+                        word_avg_len += len(word)
+                        word_cnt += 1
+                sent_avg_len /= sent_cnt
+                word_avg_len /= word_cnt
 
-            data_stat_x[category].append([word_avg_len, sent_avg_len])
+                data_stat_x[category].append([word_avg_len, sent_avg_len])
 
-pickle.dump(data_text_x, "data_text_x")
-pickle.dump(data_stat_x, "data_stat_x")
-pickle.dump(data_y, "data_y")
+    f_data_text_x = open("data_text_x", "wb")
+    f_data_stat_x = open("data_stat_x", "wb")
+    f_data_y = open("f_data_y", "wb")
+    pickle.dump(data_text_x, f_data_text_x)
+    pickle.dump(data_stat_x, f_data_stat_x)
+    pickle.dump(data_y, f_data_y)
 
 for origin_category in data_text_x:
     for target_category in data_text_x:
 
         count_vect = CountVectorizer(min_df=0, max_df=9999, binary=True, lowercase=True, stop_words=None,
                                      ngram_range=(1, 5))
+        y = data_y[origin_category]
+
         x_text = count_vect.fit_transform(data_text_x[origin_category])
         x_text = SelectKBest(chi2, k=5000).fit_transform(x_text, y)
 
@@ -65,7 +81,7 @@ for origin_category in data_text_x:
 
         x = np.concatenate((x_text, np.matrix(x_stat)), axis=1)
 
-        y = data_y[origin_category]
+
 
         # regression model
         reg = linear_model.Ridge (alpha = 1.0)
